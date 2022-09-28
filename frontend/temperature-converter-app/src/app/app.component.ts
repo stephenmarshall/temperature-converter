93b04332a77110c23e0b0a41288480b65ad42439
Stephen Marshall <stephenmarshall@outlook.com>
2022-09-28 21:37:25 +0100

    Update app.component.ts

--- frontend/temperature-converter-app/src/app/app.component.ts
+++ frontend/temperature-converter-app/src/app/app.component.ts
@@ -4,8 +4,7 @@ import { TemperatureConversion } from './TemperatureConversion';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'temperature-converter-app';
